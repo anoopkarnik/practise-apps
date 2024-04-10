a06a31dd380cd101d31b0bccd06d603207cc7420
anoopkarnik <danoopkarnik@gmail.com>
2024-04-11 03:42:46 +0530

    Removed useless apps

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NEXT_AUTH_CONFIG } from "@repo/next-auth/auth";
-import NextAuth from "next-auth/next";
-
-
-const handler = NextAuth(NEXT_AUTH_CONFIG);
-
-export {handler as GET, handler as POST}

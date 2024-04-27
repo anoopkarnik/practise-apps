d383f5fe6b38a68929a03886261af3aaadb90ccb
anoopkarnik <danoopkarnik@gmail.com>
2024-04-28 00:29:17 +0530

    used url from environment variables

--- apps/app-dashboard/app/(dasboard)/dashboard/page.tsx
+++ apps/app-dashboard/app/(dasboard)/dashboard/page.tsx
@@ -6,13 +6,13 @@ async function page() {
         {
             'title': "Paytm User App",
             'description': "A Financial app with next-auth authentication with add wallet money and transfering money functionalities",
-            'link': 'http://localhost:3001',
+            'link': process.env.PAYTM_USERAPP_URL,
             'image': 'https://raw.githubusercontent.com/anoopkarnik/turborepo-boilerplate-code/main/apps/app-dashboard/public/paytmuserclone.gif'
         },
         {
             'title': "Paytm Merchant App",
             'description': "A Financial app with next-auth authentication for google and github provider",
-            'link': 'http://localhost:3002',
+            'link': process.env.PAYTM_MERCHANTAPP_URL,
             'image': 'https://raw.githubusercontent.com/anoopkarnik/turborepo-boilerplate-code/main/apps/app-dashboard/public/paytmmerchantclone.gif'
         }
     ]

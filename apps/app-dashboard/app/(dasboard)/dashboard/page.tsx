33fcebade4ec2cf8c4408f158cac1c21c6120199
anoopkarnik <danoopkarnik@gmail.com>
2024-04-28 02:04:34 +0530

    next public added to key environment variables

--- apps/app-dashboard/app/(dasboard)/dashboard/page.tsx
+++ apps/app-dashboard/app/(dasboard)/dashboard/page.tsx
@@ -6,13 +6,13 @@ async function page() {
         {
             'title': "Paytm User App",
             'description': "A Financial app with next-auth authentication with add wallet money and transfering money functionalities",
-            'link': process.env.NEXT_PUBLIC_PAYTM_USERAPP_URL || "http://localhost:3001",
+            'link': process.env.NEXT_PUBLIC_PAYTM_USERAPP_URL || "https://paytmuserapp.practise.bsamaritan.com",
             'image': 'https://raw.githubusercontent.com/anoopkarnik/turborepo-boilerplate-code/main/apps/app-dashboard/public/paytmuserclone.gif'
         },
         {
             'title': "Paytm Merchant App",
             'description': "A Financial app with next-auth authentication for google and github provider",
-            'link': process.env.NEXT_PUBLIC_PAYTM_MERCHANTAPP_URL || "http://localhost:3002",
+            'link': process.env.NEXT_PUBLIC_PAYTM_MERCHANTAPP_URL || "https://paytmmerchantapp.practise.bsamaritan.com",
             'image': 'https://raw.githubusercontent.com/anoopkarnik/turborepo-boilerplate-code/main/apps/app-dashboard/public/paytmmerchantclone.gif'
         }
     ]
